2f0df9c881b3e5632372f4f2051f0b141f66425e
sachindavids <sachindavids@users.noreply.github.com>
2016-07-15 23:16:57 +0530

    Initial commit
    Initial commit

---
+++ js/init.js
@@ -0,0 +1,42 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+function gotoStep(num){
+
+	switch (num) {
+      case 1: 
+
+      	break;
+      case 2: 
+      	$(".step-1").addClass('step-done');
+        $(".step-2").addClass('active-step');
+      	break;
+      case 3:
+      	$(".enter-name").animate({"left":"-100%"}, function(){
+          $(this).slideUp('slow', function(){
+            $(".enter-email").slideDown('fast');
+          });
+        });
+
+      	$(".step-2").addClass('step-done');
+        $(".step-3").addClass('active-step');
+      	break;
+      case 4:
+      	$(".enter-email").animate({"left":"-100%"}, function(){
+          $(this).slideUp('slow', function(){
+            $(".select-category").slideDown('fast');
+          });
+        });
+
+      	$(".step-3").addClass('step-done');
+        $(".step-4").addClass('active-step');
+      	break;
+      default: return
+    }
+
+}
