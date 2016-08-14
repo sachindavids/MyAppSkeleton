6b63fda39b016f2a65d5275b5d9dc41483edfbaa
sachindavids <sachindavids@users.noreply.github.com>
2016-08-14 08:51:20 +0530

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -13,6 +13,12 @@ function gotoStep(num){
 
       	break;
       case 2: 
+        $(".template-cards").animate({"left":"-100%"}, function(){
+          $(this).slideUp('slow', function(){
+            $(".enter-name").slideDown('fast');
+          });
+        });
+        
       	$(".step-1").addClass('step-done');
         $(".step-2").addClass('active-step');
       	break;
@@ -35,19 +41,59 @@ function gotoStep(num){
 
       	$(".step-3").addClass('step-done');
         $(".step-4").addClass('active-step');
-        break;
-
+      	break;
       case 5:
         $(".select-category").animate({"left":"-100%"}, function(){
           $(this).slideUp('slow', function(){
-            $(".bulid-resume").slideDown('fast');
+            $(".bulid-resume").slideDown('fast', function(){
+              fnRenderBuilderResumeComponents();
+            });
           });
         });
 
         $(".step-4").addClass('step-done');
         $(".step-5").addClass('active-step');
-      	break;
+
+
+
       default: return
     }
 
+}
+
+function  fnRenderBuilderResumeComponents(){
+  var $obj = $('.table-of-contents');
+  var top = $obj.offset().top - parseFloat($obj.css('marginTop').replace(/auto/, 0));
+
+  $(window).scroll(function (event) {
+    var y = $(this).scrollTop();
+    if (y >= top) {
+      $obj.addClass('fixed');
+    } else {
+      $obj.removeClass('fixed');
+    }
+  });
+
+  $(".build-section .edit-btn").click(function(event) {
+    $(this).addClass('hide');
+    $(this).parent().find('.viewable').addClass('hide');
+    $(this).parent().find('.editable, .actions').removeClass('hide');
+  });
+
+  $(".build-section .cancel-btn").click(function(event) {
+    $(this).parent().parent().find('.viewable, .edit-btn').removeClass('hide');
+    $(this).parent().parent().find('.editable, .actions').addClass('hide');
+  });
+
+  $('#editor_summary, #editor_school_1, #editor_job_desc, #editor_hobbies, #editor_ref_desc').trumbowyg({
+    btns: ['viewHTML',
+    '|', 'formatting',
+    '|', 'btnGrp-design',
+    '|', 'link',
+    '|', 'btnGrp-justify',
+    '|', 'btnGrp-lists',
+    '|', 'horizontalRule'],
+    autogrow:true
+  });
+  
 }
