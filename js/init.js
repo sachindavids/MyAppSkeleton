a99d847f09e76a232c6b7c96e81d150bc40d4c25
sachindavids <sachindavids@users.noreply.github.com>
2016-08-26 01:50:58 +0530

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -6,41 +6,49 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-function gotoStep(num){
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
 
+function gotoStep(num){
 	switch (num) {
       case 1: 
 
       	break;
       case 2: 
-        $(".template-cards").animate({"left":"-100%"}, function(){
-          $(this).slideUp('slow', function(){
-            $(".enter-name").slideDown('fast');
+          $(".template-cards").animate({"left":"-100%"}, function(){
+            $(this).slideUp('slow', function(){
+              $(".enter-name").slideDown('fast');
+            });
           });
-        });
-        
-      	$(".step-1").addClass('step-done');
-        $(".step-2").addClass('active-step');
-      	break;
+          
+        	$(".step-1").addClass('step-done');
+          $(".step-2").addClass('active-step');
+        	break;
       case 3:
-      	$(".enter-name").animate({"left":"-100%"}, function(){
-          $(this).slideUp('slow', function(){
-            $(".enter-email").slideDown('fast');
+        if(($("#first_name").val().trim().length > 0) && ($("#last_name").val().trim().length > 0)){
+        	$(".enter-name").animate({"left":"-100%"}, function(){
+            $(this).slideUp('slow', function(){
+              $(".enter-email").slideDown('fast');
+            });
           });
-        });
 
-      	$(".step-2").addClass('step-done');
-        $(".step-3").addClass('active-step');
+        	$(".step-2").addClass('step-done');
+          $(".step-3").addClass('active-step');
+        }
       	break;
       case 4:
-      	$(".enter-email").animate({"left":"-100%"}, function(){
-          $(this).slideUp('slow', function(){
-            $(".select-category").slideDown('fast');
+        if(($("#email2").val().trim().length > 0) && validateEmail($("#email2").val())){
+        	$(".enter-email").animate({"left":"-100%"}, function(){
+            $(this).slideUp('slow', function(){
+              $(".select-category").slideDown('fast');
+            });
           });
-        });
 
-      	$(".step-3").addClass('step-done');
-        $(".step-4").addClass('active-step');
+        	$(".step-3").addClass('step-done');
+          $(".step-4").addClass('active-step');
+       }
       	break;
       case 5:
         $(".select-category").animate({"left":"-100%"}, function(){
@@ -60,6 +68,28 @@ function gotoStep(num){
     }
 
 }
+
+$.validator.setDefaults({
+    errorClass: 'invalid',
+    validClass: "valid",
+    errorPlacement: function (error, element) {
+        $(element)
+            .closest("form")
+            .find("label[for='" + element.attr("id") + "']")
+            .attr('data-error', error.text());
+    },
+    submitHandler: function (form) {
+        console.log('form ok');
+    }
+});
+
+$("#form").validate({
+    rules: {
+        dateField: {
+            date: true
+        }
+    }
+});
 
 function  fnRenderBuilderResumeComponents(){
   var $obj = $('.table-of-contents');
